d142f0df8a9ed7cfdcf32bbc291e07e101476f74
terrielama <60408333+terrielama@users.noreply.github.com>
2021-12-18 13:20:17 +0100

    Add files via upload

---
+++ dico.h
@@ -0,0 +1,10 @@
+#ifndef  dico_bit
+#define  dico_bit
+
+#include "main.h"
+
+int isLeaf(struct Noeud *arbre);
+void printCode(char *code, char c);
+void getDico(struct Noeud *arbre, char *code);
+
+#endif
