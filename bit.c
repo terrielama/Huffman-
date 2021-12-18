175e01bfefc3168e1d12761b81555f632c6c24ee
terrielama <60408333+terrielama@users.noreply.github.com>
2021-12-18 13:11:20 +0100

    Create bit.c

---
+++ bit.c
@@ -0,0 +1,31 @@
+#include "bit.h"
+
+void fromTextToBit(char *s) 
+{
+    int i;
+
+  //ouvrir le fichier
+    FILE *fp = fopen("//fichier txt", "w"); 
+
+    if (!fp)
+    {
+        printf("impossible de créer le fichier de sortie\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    int len = 0;
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+
+    for (int j = 0; j < len; j++)
+    {
+        char ch = s[j];
+        for (i = 7; i >= 0; --i)
+            (ch & 1 << i) ? fputc('1', fp) : fputc('0', fp);
+    }
+//fermeture
+    fclose(fp); 
+}
