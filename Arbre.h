afce31b50216aecdf9d7feede2dd164e7a06ba92
terrielama <60408333+terrielama@users.noreply.github.com>
2022-01-09 07:10:47 +0100

    Update Arbre.h

--- Arbre.h
+++ Arbre.h
@@ -3,18 +3,18 @@
 //fichier header
 #include "main.h"
 
-typedef Node* Arbre;
+typedef Noeud* Arbre;
 
 //les prototypes des fonctions.
 void affiche_arbre(Arbre arbre);
-Node* creation_node_arbre(char val, int occ);
-void freeArbre(struct Node *arbre);
-void fusionDesDeuxMin(struct Liste_Node **huffLiset);
-Liste_Node* huffman(struct List_Node* liste);
-Liste_Node *huffList(struct Liste *liste, int i);
-void afficheHuffmanListe(struct Liste_Node *huffListe);
-Liste_Node *creationNodeListe(struct Node *arbre, int i);
-void envoi(struct Liste_Node** t_ref, Node *nouv_donnee, int i);
+Noeud* creation_noeud_arbre(char val, int occ);
+void freeArbre(struct Noeud *arbre);
+void fusionDesDeuxMin(struct Liste_Noeud **huffLiset);
+Liste_Noeud* huffman(struct Liste_Noeud* liste);
+Liste_Noeud *huffList(struct Liste *liste, int i);
+void afficheHuffmanListe(struct Liste_Noeud *huffListe);
+Liste_Noeud *creationNoeudListe(struct Noeud *arbre, int i);
+void envoi(struct Liste_Noeud** t_ref, Noeud *nouv_donnee, int i);
 
 #endif
 
