90934ce858d19f45d31bddae806ce96367929c82
terrielama <60408333+terrielama@users.noreply.github.com>
2022-01-09 07:07:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,149 @@
-#ifndef  h_arbre
-#define  h_arbre
+#include "main.h"
 
-#include "main.c"
+int main(int argc, char *argv[])
+{
 
-typedef Node* Arbre;
 
-void affiche_arbre(Arbre arbre);
-Node* creation_node_arbre(char val, int occ);
-void freeArbre(struct Node *arbre);
-void mergeTheTwoSmallest(struct Liste_Node **huffListe);
-List_Node* huffman(struct Liste_Node* liste);
-List_Node *huffListe(struct Liste *liste, int i);
-void afficheHuffmanListe(struct Liste_Node *huffListe);
-List_Node *creationNodeListe(struct Node *arbre, int i);
-void envoi(struct Liste_Node** t_ref, Node *nouv_donnee, int i);
+    (void)argc;
 
-#endif
+    char *filename = argv[1];
+
+    ecrireOccur(filename);
+
+    char *st;
+    st = fileToString(filename);
+    fromTextToBit(st);
+    free(st);
+
+    FILE *f = fopen("data/occur.txt", "r");
+    int compteur = 0;
+    int first = 0;
+    for (char c = getc(f); c != EOF; c = getc(f))
+    {
+        if (!first && c == '\n')
+        {
+            first = 1;
+            continue;
+        }
+
+        first = 1;
+
+
+        if (c == '\n')
+        {
+            compteur++;
+        }
+    }
+    counter++;
+
+    //printf("compteur = %d\n", compteur);
+
+    fclose(f);
+
+    Liste *resultat = creer_liste(compteur);
+    listeOccur(resultat, "data/occur.txt");
+
+    //printListe(resultat);
+
+    Liste_Noeud *huffmanListe = huffListe(resultat, 1);
+    printf("\n");
+
+
+
+    Noeud *huffArbre = (huffman(huffmanListe))->data;
+
+    //print_tree(huffTree);
+
+    char code[256] = {0};
+
+    remove("data/dico.txt");
+    getDico(huffTree, code);
+
+    encode(filename);
+}
+
+char *fileToString(const char *filename)
+{
+    char *s;
+    int length;
+    FILE *f = fopen(filename, "r");
+    if (f == NULL)
+    {
+        return NULL; // tu ne peux plus rien faire tout va crash
+    }
+
+    if (fseek(f, 0, SEEK_END) < 0 || (length = ftell(f)) < 0 || fseek(f, 0, SEEK_SET) < 0)
+    {
+        fclose(f);
+        return NULL; // tu ne peux plus rien faire tout va crash
+    }
+    s = malloc((length + 1) * sizeof(*s));
+    if (s == NULL)
+    {
+        fclose(f);
+        return NULL; // tu ne peux plus rien faire tout va crash
+    }
+    s[length] = '\0';
+    if ((unsigned long)length != fread(s, 1, length, f))
+    {
+        free(s);
+        fclose(f);
+        return NULL; // tu ne peux plus rien faire tout va crash
+    }
+    fclose(f);
+
+    return s;
+}
+
+void printListe(struct Liste *liste) //Print a linked list
+{
+    while (liste != NULL)
+    {
+        if (liste)
+        {
+            printf("%c = %ld,\n", liste->c, liste->occur);
+        }
+
+        if (!liste->suivant)
+        {
+            break;
+        }
+
+        liste = liste->suivant;
+    }
+}
+
+void supprNoeud(struct Liste_Noeud **ref, int key)
+{
+    if (!*ref)
+    {
+        return;
+    }
+
+    struct Liste_Noeud *temp = *ref;
+    struct Liste_Noeud *prev = NULL;
+
+    if (temp != NULL && temp->id == key)
+    {
+        *ref = temp->suivant;
+        free(temp);
+        temp = NULL;
+        return;
+    }
+    //Trouver la clé à supprimer
+    while (temp != NULL && temp->id != key)
+    {
+        prev = temp;
+        temp = temp->suivant;
+    }
+
+    // si la clé n'est pas présente
+    if (temp == NULL)
+        return;
+
+    // suppr Noeud
+    prev->suivant = temp->suivant;
+
+    free(temp);
+    temp = NULL;
+}
